939865af4c62a677980844bda9ceac4ff2d8a9ef
wipeautcrafter <wipeautcrafter@gmail.com>
2017-12-30 14:12:25 +0100

    Added loop.js
    loop.js is a library that makes it easier to create loops in your JS and NodeJS libraries

---
+++ loop.js
@@ -0,0 +1,18 @@
+// usage: loop(<function>, <delay>, [amount], [callback]);
+module.exports = loop;
+
+var loop = function(f, d, a, c) {
+  if(!f) throw "Invalid Syntax: Missing Function.";
+  if(!d) throw "Invalid Syntax: Missing Delay.";
+  if(!f || !d) return;
+  
+  if(a) {
+    for(var i = 0; i < a; i++) {
+      setTimeout(f, i*d);
+    }
+    
+    if(c) setTimeout(c, i*d);
+  } else {
+    setInterval(f, d);
+  }
+};
