eb319dc94d4c90742803c2cbf7d588d2403f75e3
wipeautcrafter <wipeautcrafter@gmail.com>
2017-12-30 14:25:10 +0100

    Update loop.js

--- loop.js
+++ loop.js
@@ -1,18 +1,1 @@
-// usage: loop(<function>, <delay>, [amount], [callback]);
-var loop = function(f, d, a, c) {
-  if(!f) throw "Invalid Syntax: Missing Function.";
-  if(!d) throw "Invalid Syntax: Missing Delay.";
-  if(!f || !d) return;
-  
-  if(a) {
-    for(var i = 0; i < a; i++) {
-      setTimeout(f, i*d);
-    }
-    
-    if(c) setTimeout(c, i*d);
-  } else {
-    setInterval(f, d);
-  }
-};
-
-module.exports = loop;
+var loop=function(t,i,o,e){if(!t)throw"Invalid Syntax: Missing Function.";if(!i)throw"Invalid Syntax: Missing Delay.";if(t&&i)if(o){for(var n=0;n<o;n++)setTimeout(t,n*i);e&&setTimeout(e,n*i)}else setInterval(t,i)};try{module.exports=loop}catch(t){}
