c6b5155abaa81302e781531d37f8b6402b61a622
wipeautcrafter <wipeautcrafter@gmail.com>
2018-02-02 19:39:50 +0100

    Update httpget.js

--- httpget.js
+++ httpget.js
@@ -1,1 +1,1 @@
-module.exports=function(t,o){require(t.startsWith("https")?"https":"http").get(t,function(t){var n="";t.on("data",t=>{n+=t,console.log(n)}),t.on("end",()=>o(n))})};
+module.exports=function(t,o){require(t.startsWith("https")?"https":"http").get(t,function(t){var n="";t.on("data",t=>{n+=t}),t.on("end",()=>o(n))})};
