48b5169d92d996e223f7fc2e2d7da501330dd563
wipeautcrafter <wipeautcrafter@gmail.com>
2018-02-02 19:36:00 +0100

    you can now make https requests

--- httpget.js
+++ httpget.js
@@ -1,1 +1,1 @@
-module.exports=get=function(o,t,n){require("http").get({host:o,path:t},function(o){var t="";o.on("data",o=>{t+=o,console.log(t)}),o.on("end",()=>n(t))})};
+module.exports=function(t,o){require(t.startsWith("https")?"https":"http").get(t,function(t){var n="";t.on("data",t=>{n+=t,console.log(n)}),t.on("end",()=>o(n))})};
